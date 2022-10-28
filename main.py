744cf121ef23b472e11114fefe8efe22842549c0
Ethan Vinh <114762393+ethanfirst@users.noreply.github.com>
2022-10-28 15:52:30 +0000

    Added a level of recursion to calculate any number

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 def main():
     my_fib = Fib()
-    print(my_fib.fib(4))
-
+   
+    print(my_fib.fib(10)) 
+ 
 class Fib:
-    beginning_nums = (0, 1, 1, 2, 3, 5, 8)
+    beginning_nums = (0, 1, 1, 2, 3, 5, 8) 
+    beginning_nums = (0, 1, 1) 
     def fib(self, in_num):
-        return self.beginning_nums[in_num]
+        
+        if in_num < 3:
+            return self.beginning_nums[in_num] 
+        return self.fib(in_num - 1) + self.fib(in_num - 2) 
 
-main()
 
